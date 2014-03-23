13359b84122f132ee109599b377710bd4b842d5c
Alejandra G. <ale7jandra.89@gmail.com>
2014-03-23 19:45:24 +0100

    Cambios parciales en index de farmacos.
    Plantilla con varios items.

--- rescueincloud/protected/views/farmacos/index.php
+++ rescueincloud/protected/views/farmacos/index.php
@@ -1,94 +1,135 @@
 <div class="row clearfix">
-		<div class="col-md-12 column">
-			<table class="table">
-				<thead>
-					<tr>
-						<th>
-							#
-						</th>
-						<th>
-							Product
-						</th>
-						<th>
-							Payment Taken
-						</th>
-						<th>
-							Status
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>
-							1
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							01/04/2012
-						</td>
-						<td>
-							Default
-						</td>
-					</tr>
-					<tr class="active">
-						<td>
-							1
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							01/04/2012
-						</td>
-						<td>
-							Approved
-						</td>
-					</tr>
-					<tr class="success">
-						<td>
-							2
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							02/04/2012
-						</td>
-						<td>
-							Declined
-						</td>
-					</tr>
-					<tr class="warning">
-						<td>
-							3
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							03/04/2012
-						</td>
-						<td>
-							Pending
-						</td>
-					</tr>
-					<tr class="danger">
-						<td>
-							4
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							04/04/2012
-						</td>
-						<td>
-							Call in to confirm
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
+    <div class="col-md-2 column">
+        
+        <ul class="nav nav-pills nav-stacked">
+          <li class="nav-header"></li>
+          <li><a href="#"><i class="glyphicon glyphicon-user"></i> Mis Fármacos</a></li>
+          <li><a href="#"><i class="glyphicon glyphicon-plus"></i> Crear Fármaco</a></li>
+          <li><a href="#"><i class="glyphicon glyphicon-remove"></i> Eliminar Fármaco</a></li>
+          <li><a href="#"><i class="glyphicon glyphicon-search"></i> Buscar Fármaco</a></li>
+        </ul>
+
+    </div>
+  
+    <!-- TODO: 
+        1. Enlazar los enlaces anteriores con su respectiva vista.
+        2. Generar la tabla con datos reales.
+    -->
+    
+    <div class="col-md-10 column">
+        <hr>   
+        <table class="table table-hover table-bordered">
+                    <thead>
+                            <tr>
+                                    <th>
+                                            #
+                                    </th>
+                                    <th>
+                                            Product
+                                    </th>
+                                    <th>
+                                            Payment Taken
+                                    </th>
+                                    <th>
+                                            Status
+                                    </th>
+                            </tr>
+                    </thead>
+                    <tbody>
+                            <tr>
+                                    <td>
+                                            1
+                                    </td>
+                                    <td>
+                                            TB - Monthly
+                                    </td>
+                                    <td>
+                                            01/04/2012
+                                    </td>
+                                    <td>
+                                            Default
+                                    </td>
+                            </tr>
+                            <tr class="active">
+                                    <td>
+                                            1
+                                    </td>
+                                    <td>
+                                            TB - Monthly
+                                    </td>
+                                    <td>
+                                            01/04/2012
+                                    </td>
+                                    <td>
+                                            Approved
+                                    </td>
+                            </tr>
+                            <tr class="success">
+                                    <td>
+                                            2
+                                    </td>
+                                    <td>
+                                            TB - Monthly
+                                    </td>
+                                    <td>
+                                            02/04/2012
+                                    </td>
+                                    <td>
+                                            Declined
+                                    </td>
+                            </tr>
+                            <tr class="warning">
+                                    <td>
+                                            3
+                                    </td>
+                                    <td>
+                                            TB - Monthly
+                                    </td>
+                                    <td>
+                                            03/04/2012
+                                    </td>
+                                    <td>
+                                            Pending
+                                    </td>
+                            </tr>
+                            <tr class="danger">
+                                    <td>
+                                            4
+                                    </td>
+                                    <td>
+                                            TB - Monthly
+                                    </td>
+                                    <td>
+                                            04/04/2012
+                                    </td>
+                                    <td>
+                                            Call in to confirm
+                                    </td>
+                            </tr>
+                    </tbody>
+            </table>
+            <ul class="pagination pagination-sm">
+                    <li>
+                            <a href="#">Prev</a>
+                    </li>
+                    <li>
+                            <a href="#">1</a>
+                    </li>
+                    <li>
+                            <a href="#">2</a>
+                    </li>
+                    <li>
+                            <a href="#">3</a>
+                    </li>
+                    <li>
+                            <a href="#">4</a>
+                    </li>
+                    <li>
+                            <a href="#">5</a>
+                    </li>
+                    <li>
+                            <a href="#">Next</a>
+                    </li>
+            </ul>
+    </div>
+</div>
