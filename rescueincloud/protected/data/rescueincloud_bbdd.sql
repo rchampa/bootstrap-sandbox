aca617a4b92aaf3bf7652eb92d22447eae061fb1
Ricardo <ricardochampa@gmail.com>
2014-05-25 18:30:31 +0200

    bbdd actualizada

--- rescueincloud/protected/data/rescueincloud_bbdd.sql
+++ rescueincloud/protected/data/rescueincloud_bbdd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 23-05-2014 a las 01:15:26
+-- Tiempo de generación: 25-05-2014 a las 18:13:44
 -- Versión del servidor: 5.5.36
 -- Versión de PHP: 5.4.27
 
@@ -42,8 +42,55 @@ CREATE TABLE IF NOT EXISTS `cajatexto` (
 INSERT INTO `cajatexto` (`id_caja_texto`, `tipo`, `contenido`, `id_protocolo`) VALUES
 (0, 0, 'canción de fuego', 13),
 (0, 0, 'canción de arena', 14),
+(0, 0, 'llal', 15),
+(0, 0, 'algo fácil', 16),
+(0, 0, '¿cómo falla esto?', 17),
+(0, 0, 'canción de fuego', 18),
+(0, 0, 'dos', 19),
+(0, 0, 'tres', 20),
+(0, 0, 'cuatro', 21),
+(0, 0, 'seis', 22),
+(0, 0, 'canción de arena', 23),
+(0, 0, 'prueba Ricardo', 24),
 (1, 0, 'palabras con tildes: Lucía', 13),
-(1, 0, 'já jaá', 14);
+(1, 0, 'já jaá', 14),
+(1, 1, 'pregunta?', 15),
+(1, 0, 'facil1', 16),
+(1, 0, 'esto falla forque...', 17),
+(1, 0, 'palabras con tildes: Lucía', 18),
+(1, 0, 'lala', 19),
+(1, 0, 'jajajaj', 20),
+(1, 0, 'kkkkk', 21),
+(1, 0, 'respect the people', 22),
+(1, 0, 'já jaá', 23),
+(1, 1, 'esta prueba funciona?', 24),
+(2, 1, 'potra pregunta', 15),
+(2, 0, 'facil2', 16),
+(2, 1, 'Rajoy es un inútil?', 17),
+(2, 0, 'uno', 18),
+(2, 0, 'pedido', 23),
+(2, 0, 'si, lo hace', 24),
+(3, 0, 'jajajaj?', 15),
+(3, 0, 'y edtiableeeeee', 16),
+(3, 0, 'Que va...', 17),
+(3, 0, 'pero esto funciona bien?', 23),
+(3, 1, 'falta probar', 24),
+(4, 0, 'pero esto que és?', 15),
+(4, 1, 'y qué más?', 17),
+(4, 0, 'sip', 23),
+(4, 0, 'el qué?', 24),
+(5, 1, 'cómo?¿¿?', 15),
+(5, 1, 'seguro', 23),
+(6, 1, 'como se ve?', 15),
+(6, 0, 'pues si', 23),
+(7, 1, 'esto funciona?', 15),
+(7, 0, 'que va...', 23),
+(8, 1, 'si que funciona?', 15),
+(9, 0, 'no que va', 15),
+(10, 1, 'como que no?', 15),
+(11, 1, 'porque noooo', 15),
+(12, 0, 'pues nooo', 15),
+(13, 0, 'que sí hombre!', 15);
 
 -- --------------------------------------------------------
 
@@ -59,7 +106,7 @@ CREATE TABLE IF NOT EXISTS `cajatexto_hijos` (
   `relacion` tinyint(2) NOT NULL,
   PRIMARY KEY (`id`,`id_protocolo`),
   KEY `fk_CajaTexto_has_CajaTexto1_CajaTexto1` (`id_protocolo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=33 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=94 ;
 
 --
 -- Volcado de datos para la tabla `cajatexto_hijos`
@@ -69,7 +116,51 @@ INSERT INTO `cajatexto_hijos` (`id`, `id_protocolo`, `id_hijo`, `id_padre`, `rel
 (29, 13, 0, 0, 0),
 (30, 13, 1, 0, 2),
 (31, 14, 0, 0, 0),
-(32, 14, 1, 0, 2);
+(32, 14, 1, 0, 2),
+(50, 17, 0, 0, 0),
+(51, 17, 1, 0, 2),
+(52, 17, 2, 1, 2),
+(53, 17, 3, 2, 1),
+(54, 17, 4, 2, 0),
+(55, 18, 1, 0, 2),
+(56, 18, 2, 0, 0),
+(57, 19, 0, 0, 0),
+(58, 19, 1, 0, 2),
+(59, 20, 0, 0, 0),
+(60, 20, 1, 0, 2),
+(61, 21, 0, 0, 0),
+(62, 21, 1, 0, 2),
+(63, 22, 0, 0, 0),
+(64, 22, 1, 0, 2),
+(65, 23, 1, 0, 2),
+(66, 23, 2, 0, 0),
+(67, 23, 3, 0, 2),
+(68, 23, 4, 3, 2),
+(69, 23, 5, 4, 2),
+(70, 23, 6, 5, 0),
+(71, 23, 7, 5, 1),
+(72, 24, 0, 0, 0),
+(73, 24, 1, 0, 2),
+(74, 24, 2, 1, 0),
+(75, 24, 3, 1, 1),
+(76, 24, 4, 3, 1),
+(77, 16, 1, 0, 2),
+(78, 16, 2, 1, 2),
+(79, 16, 3, 2, 2),
+(80, 15, 1, 0, 2),
+(81, 15, 2, 1, 0),
+(82, 15, 3, 1, 1),
+(83, 15, 4, 2, 0),
+(84, 15, 5, 2, 1),
+(85, 15, 6, 4, 2),
+(86, 15, 7, 6, 0),
+(87, 15, 8, 7, 0),
+(88, 15, 9, 8, 1),
+(89, 15, 10, 9, 2),
+(90, 15, 11, 10, 1),
+(91, 15, 12, 11, 1),
+(92, 15, 13, 11, 0),
+(93, 15, 13, 12, 0);
 
 -- --------------------------------------------------------
 
@@ -178,10 +269,11 @@ CREATE TABLE IF NOT EXISTS `protocolos` (
   `nombre_protocolo` varchar(45) NOT NULL,
   `email_usuario` varchar(45) NOT NULL,
   `codigo_parseado` text NOT NULL,
+  `borrado` tinyint(1) NOT NULL DEFAULT '0',
   `creado_en` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `actualizado_en` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_protocolo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
 
 --
 -- Volcado de datos para la tabla `protocolos`
@@ -190,7 +282,17 @@ CREATE TABLE IF NOT EXISTS `protocolos` (
 INSERT INTO `protocolos` (`id_protocolo`, `nombre_protocolo`, `email_usuario`, `codigo_parseado`, `creado_en`, `actualizado_en`) VALUES
 (1, 'manejo de la hipotermia accidental severa', 'ricardocb48@gmail.com', '', '2014-05-05 17:52:11', '2014-05-13 13:04:58'),
 (13, 'canción de fuego', 'ricardocb48@gmail.com', '1=>operation: palabras con tildes: Lucía\r\nst=>start: canción de fuego\r\n\r\nst->1\r\n', '2014-05-22 21:43:55', '2014-05-22 21:43:55'),
-(14, 'canción de arena', 'ricardocb48@gmail.com', '1=>operation: já jaá\r\nst=>start: canción de arena\r\n\r\nst->1\r\n', '2014-05-22 21:48:19', '2014-05-22 21:48:19');
+(14, 'canción de arena', 'ricardocb48@gmail.com', '1=>operation: já jaá\r\nst=>start: canción de arena\r\n\r\nst->1\r\n', '2014-05-22 21:48:19', '2014-05-22 21:48:19'),
+(15, 'llal', 'ricardocb48@gmail.com', '13=>operation: que sí hombre!\r\n12=>operation: pues nooo\r\ncond11=>condition: porque noooo\r\ncond10=>condition: como que no?\r\n9=>operation: no que va\r\ncond8=>condition: si que funciona?\r\ncond7=>condition: esto funciona?\r\ncond6=>condition: como se ve?\r\ncond5=>condition: cómo?¿¿?\r\n4=>operation: pero esto que és?\r\n3=>operation: jajajaj?\r\ncond2=>condition: potra pregunta\r\ncond1=>condition: pregunta?\r\nst=>start: llal\r\n\r\nst->cond1\r\ncond1(yes, right)->cond2\r\ncond1(no)->3\r\ncond2(yes, right)->4\r\ncond2(no)->cond5\r\n4->cond6\r\ncond6(yes, right)->cond7\r\ncond7(yes, right)->cond8\r\ncond8(no)->9\r\n9->cond10\r\ncond10(no)->cond11\r\ncond11(no)->12\r\ncond11(yes, right)->13\r\n12->13\r\n', '2014-05-24 18:04:13', '2014-05-25 11:37:12'),
+(16, 'algo fácil', 'ricardocb48@gmail.com', '3=>operation: y edtiableeeeee\r\n2=>operation: facil2\r\n1=>operation: facil1\r\nst=>start: algo fácil\r\n\r\nst->1\r\n1->2\r\n2->3\r\n', '2014-05-24 18:06:04', '2014-05-25 11:20:06'),
+(17, '¿cómo falla esto?', 'ricardocb48@gmail.com', 'cond4=>condition: y qué más?\r\n3=>operation: Que va...\r\ncond2=>condition: Rajoy es un inútil?\r\n1=>operation: esto falla forque...\r\nst=>start: ¿cómo falla esto?\r\n\r\nst->1\r\n1->cond2\r\ncond2(no)->3\r\ncond2(yes, right)->cond4\r\n', '2014-05-24 18:11:38', '2014-05-24 18:11:38'),
+(18, 'canción de fuego', 'ricardocb48@gmail.com', 'st=>start: uno\r\n\r\nst->', '2014-05-25 08:57:54', '2014-05-25 08:57:54'),
+(19, 'dos', 'ricardocb48@gmail.com', '1=>operation: lala\r\nst=>start: dos\r\n\r\nst->1\r\n', '2014-05-25 08:58:26', '2014-05-25 08:58:26'),
+(20, 'tres', 'ricardocb48@gmail.com', '1=>operation: jajajaj\r\nst=>start: tres\r\n\r\nst->1\r\n', '2014-05-25 08:58:44', '2014-05-25 08:58:44'),
+(21, 'cuatro', 'ricardocb48@gmail.com', '1=>operation: kkkkk\r\nst=>start: cuatro\r\n\r\nst->1\r\n', '2014-05-25 08:59:02', '2014-05-25 08:59:02'),
+(22, 'seis', 'ricardocb48@gmail.com', '1=>operation: respect the people\r\nst=>start: seis\r\n\r\nst->1\r\n', '2014-05-25 08:59:20', '2014-05-25 08:59:20'),
+(23, 'canción de arena', 'ricardocb48@gmail.com', '7=>operation: que va...\r\n6=>operation: pues si\r\ncond5=>condition: seguro\r\n4=>operation: sip\r\n3=>operation: pero esto funciona bien?\r\nst=>start: pedido\r\n\r\nst->3\r\n3->4\r\n4->cond5\r\ncond5(yes, right)->6\r\ncond5(no)->7\r\n', '2014-05-25 09:35:33', '2014-05-25 09:35:33'),
+(24, 'prueba Ricardo', 'ricardocb48@gmail.com', '4=>operation: el qué?\r\ncond3=>condition: falta probar\r\n2=>operation: si, lo hace\r\ncond1=>condition: esta prueba funciona?\r\nst=>start: prueba Ricardo\r\n\r\nst->cond1\r\ncond1(yes, right)->2\r\ncond1(no)->cond3\r\ncond3(no)->4\r\n', '2014-05-25 09:37:52', '2014-05-25 09:37:52');
 
 -- --------------------------------------------------------
 
