6eca26d9fddcb096464ecf533df32f5d4b519bcc
Ricardo <ricardochampa@gmail.com>
2014-05-28 00:46:57 +0200

    nNombre de bbdd actualizado

--- rescueincloud/protected/config/main.php
+++ rescueincloud/protected/config/main.php
@@ -52,7 +52,7 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=rescueincloud;charset=utf8',
+			'connectionString' => 'mysql:host=localhost;dbname=ems;charset=utf8',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
--- rescueincloud/themes/layoutit/views/layouts/plantilla.php
+++ rescueincloud/themes/layoutit/views/layouts/plantilla.php
@@ -3,7 +3,7 @@
 <html lang="es">
 <head>
   <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
-  <title>MS in cloud</title>
+  <title>EMS in cloud</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
