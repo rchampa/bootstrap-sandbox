668232f67a95f41778c88d8d7240c17f852b4bd3
Ricardo <ricardochampa@gmail.com>
2014-05-28 02:01:56 +0200

    Añadido el servicio web

---
+++ rescueincloud/ws/scripts/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Database config variables
+ */
+/*
+define("DB_HOST", "mysql.hostinger.es");
+define("DB_USER", "u292523446_mem1");
+define("DB_PASSWORD", "ios.weekend18");
+define("DB_DATABASE", "u292523446_mem");
+*/
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "ems");
+ 
+?>
